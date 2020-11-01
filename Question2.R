78f8f5cb1a3032326c8ea6613f0e8d86aa415b63
Sehazen <sehazen56@gmail.com>
2020-11-01 12:08:47 -0500

    Updated Question 2: Refined Discussion

--- Question2.R
+++ Question2.R
@@ -18,7 +18,10 @@ ggplot(data=data1, aes(x=region, y=observations))+
 #Both graphs show that the mean for each region is approx. 15.
 #But the scatterplot shows that each region has their own unique
 #distribution. The eastern observations are grouped around 15 but
-# have a large spread. The northern observations are tightly packed
+#have a large spread. The northern observations are tightly packed
 #around 15 with little to no spread. The southern observations have
 #two distinct groups at 25 and 5 that average to 15,and the western
-#observations are approx. uniformly distributed beween 0 and 30.
+#observations are approx. uniformly distributed between 0 and 30.
+#Thus providing more information about each region than the bar plot
+#alone. The barplot shows only the means, while the scatter shows
+#the distribtuions that led to those means.
